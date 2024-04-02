5873dd5fbd71333fc0a83647cb6287f0d558c414
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-02 11:04:50 +0200

    playing a bit with javascript

--- src/home.js
+++ src/home.js
@@ -3,15 +3,31 @@ const Home = () =>{
 const handleClick = (e) => {
     console.log('hi')
 }
+let show =true
 
 const handleClickAgain = (name,e) => {
     console.log('hi' + ' ' + name,e)
+   let div = document.getElementsByClassName('div')[0]
+show=!show
+if(show){
+    if(div!==undefined && div!== null){
+div.classList.add('hide')
+}
+}else{
+    if(div!==undefined && div!== null){
+        div.classList.remove('hide')
+        }  
+}
+
 }
 
 return (
     <div className="Home">
         <h1 onClick={handleClick}>Home</h1>
         <button onClick={(e)=> {handleClickAgain('Raffaele',e)}}>Say hi to me</button>
+        <div className="div">
+            Ciao
+        </div>
     </div>
 );
 }
