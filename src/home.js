34a4b68f1c5877aeba6081a4d9a17cd07d7ac69d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-02 10:52:28 +0200

    handling click events

--- src/home.js
+++ src/home.js
@@ -1,7 +1,17 @@
 const Home = () =>{
+
+const handleClick = (e) => {
+    console.log('hi')
+}
+
+const handleClickAgain = (name,e) => {
+    console.log('hi' + ' ' + name,e)
+}
+
 return (
     <div className="Home">
-        <h1>Home</h1>
+        <h1 onClick={handleClick}>Home</h1>
+        <button onClick={(e)=> {handleClickAgain('Raffaele',e)}}>Say hi to me</button>
     </div>
 );
 }
