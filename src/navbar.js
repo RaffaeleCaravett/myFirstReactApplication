a64dd692f248806086d78a18a1a72c3c62a962f0
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-06 21:07:43 +0200

    changes a tags to Links

--- src/navbar.js
+++ src/navbar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const Navbar = () => {
     
     const title = "My first react app"
@@ -6,8 +8,8 @@ const Navbar = () => {
         <nav className="navbar">
         <h1>{title}</h1>
         <div className="links">
-<a href="/">Home</a>
-<a href="/create">New Blog</a>
+<Link to="/">Home</Link>
+<Link to="/create">New Blog</Link>
         </div>
         </nav>
     )
