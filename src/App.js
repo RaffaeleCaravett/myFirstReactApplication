03b896c422307a133c46ea2dcc7e3f38c9b11876
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-06 20:47:33 +0200

    added route '/'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Navbar from './navbar';
-import Home from './home'
+import Home from './home';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
 <Navbar  className="navbar"/>
       <div className="Content">
+        <Switch>
+          <Route path="/">
 <Home />
+          </Route>
+        </Switch>
+
       </div>
     </div>
+    </Router>
   );
 }
 
