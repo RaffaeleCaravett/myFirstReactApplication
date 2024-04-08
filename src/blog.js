3bd86fedf3936b54f4aafbda03b5731e54c06ecc
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-08 10:05:05 +0200

    getting id by route params

--- src/blog.js
+++ src/blog.js
@@ -1,9 +1,15 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
 const Blog = () => {
 
+const { id } = useParams()
 
 return (
     <div className="blogs">
         <h1>Blog Component</h1>
+        <div>
+           <p>Now showing blog with id : {id}</p> 
+        </div>
     </div>
 )
 }
