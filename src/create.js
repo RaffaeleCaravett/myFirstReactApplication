bcab67788eb0bd3a62aeefaf2c3d5e3342f61480
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-08 10:39:03 +0200

    now doing the form

--- src/create.js
+++ src/create.js
@@ -1,6 +1,26 @@
 const Create = () =>{
 
     
+
+    return (
+        <div className="formDiv">
+            <h1>Add a blog</h1>
+<form>
+    <label>Inserisci il titolo del blog :</label>
+    <input 
+    type="text"
+    required/>
+    <label>Inserisci il testo del blog :</label>
+    <input 
+    type="text"
+    required/>
+    <label>Inserisci l'autore :</label>
+    <input 
+    type="text"
+    required/>
+</form>
+        </div>
+    )
 }
 
 export default Create;
