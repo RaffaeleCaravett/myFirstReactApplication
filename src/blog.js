2173c8bb8e6704ee9bf4d3273442acd39a6556a7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-08 10:22:37 +0200

    succesfuly getting a blog

--- src/blog.js
+++ src/blog.js
@@ -1,15 +1,25 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch"
 
 const Blog = () => {
 
 const { id } = useParams()
 
+const {data,isPending,error} = useFetch(`http://localhost:8000/blog/${id}`)
+
 return (
     <div className="blogs">
         <h1>Blog Component</h1>
         <div>
-           <p>Now showing blog with id : {id}</p> 
+           {!error && <p>Now showing blog with id : {id}</p>} 
+           {isPending && <p> Loading ...</p>}
+           {error && <p> C'è stato un errore nel recupero del blog</p>}
         </div>
+        {data && <div className="blog-preview" key={data.id}>
+    <h1>{data.title}</h1>
+    <p>{data.body}</p><span>{data.author}</span>   
+    
+</div>}
     </div>
 )
 }
